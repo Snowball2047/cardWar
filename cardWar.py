c087d0730c7a030199dd9f2ab0e56544ed5b19b6
Louis Icke <Snowball2047@users.noreply.github.com>
2017-01-27 20:08:58 +0000

    Added player class and deal function

--- cardWar.py
+++ cardWar.py
@@ -1,15 +1,40 @@
 import random
 
 def createDeck():
-    length = ''
-    while length != '3' or length != '6' or length != '10':
-        length = input('What size do you want the deck to be? (It has to be 3, 6, or 10')
-    if length == 3:
-        deck = ['1','2','3']
-    elif length == 6:
-        deck = ['1','2','3','4','5','6']
-    else:
-        deck = ['1','2','3','4','5','6','7','8','9','10']
-    return deck
+	suits = ['S','H','C','D']
+	deck = []
+	length = ''
+	standard = input('Do you want to use a standard deck?\n')
+	if standard.upper() == 'YES':
+		numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+	else:
+		while length != '12' and length != '24' and length != '40':
+			length = input('What size do you want the deck to be? (It has to be 12, 24, or 40)\n')
+		if length == 3:
+			numbers = ['A','2','3']
+		elif length == 6:
+			numbers = ['A','2','3','4','5','6']
+		else:
+			numbers = ['A','2','3','4','5','6','7','8','9','10']
+	for i in range(0,4):
+		tempSuit = suits[i]
+		for i in range(0,len(numbers)):
+			card = tempSuit +numbers[i]
+			deck.append(card)
+	return deck
 
+class player():
+	deck = createDeck()
+	def __init__(player):
+		deck = player.deck
+		cardNum = random.randint(0,len(deck))
+		player.card = deck[cardNum]
 
+def deal():
+	playerNum = int(input('How many players are there?\n'))
+	players = []
+	for i in range(0,playerNum):
+		players.append(player())
+	return players
+
+players = deal()
